dcb1c6455e7642977c1391db4e9c5122d9337ae7
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-05-09 20:21:06 +0530

    fix few bugs

--- getCats/src/components/Card.jsx
+++ getCats/src/components/Card.jsx
@@ -47,11 +47,18 @@ function Card({ cat }) {
           {isFavorited ? (
             <span className="text-white text-base leading-none">♥</span>
           ) : (
-            <img
-              src="src/assets/heart.png"
-              alt="Favorite"
-              className="w-4.5 h-4.5 object-contain"
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+            </svg>
           )}
         </button>
       </div>
